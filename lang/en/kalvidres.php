7a71f8df64ed552957c3823ac4ecc19aec59ca71
Akinsaya Delamarre <adelamarre@remote-learner.net>
2012-12-12 14:28:21 -0500

    Adding 2.x code

---
+++ lang/en/kalvidres.php
@@ -0,0 +1,27 @@
+<?php
+
+$string['modulenameplural'] = 'Kaltura Video Resource';
+$string['modulename'] = 'Kaltura Video Resource';
+$string['pluginadministration'] = 'Kaltura Video Resource';
+$string['pluginname'] = 'Kaltura Video Resource';
+$string['name'] = 'Name';
+$string['video_hdr'] = 'Video';
+$string['add_video'] = 'Add video';
+$string['video_properties'] = 'Player properties';
+$string['upload_successful'] = 'Upload Successful';
+$string['video_converting'] = 'Your video is still converting.  Please check the status of your video at a later time.';
+$string['vid_prop_name'] = 'Name:';
+$string['vid_prop_player'] = 'Player design:';
+$string['vid_prop_dimensions'] = 'Player dimensions:';
+$string['vid_prop_size'] = 'Player size:';
+$string['vid_prop_size_large'] = 'Large (400x365)';
+$string['vid_prop_size_small'] = 'Small (260x260)';
+$string['vid_prop_size_custom'] = 'Custom size';
+$string['normal'] = 'Normal';
+$string['vide_preview'] = 'Preview';
+$string['widescreen'] = 'Widescreen';
+$string['vid_prop_header'] = 'Player Properties';
+$string['video_preview_header'] = 'Preview';
+$string['invalidid'] = 'Invalid ID';
+$string['replace_video'] = 'Replace video';
+$string['custom_player'] = 'Custom player';
